588626f00436dbff3a35f6e3952a38357cc82da5
Lapets_Dmitry <veles-hj@mail.ru>
2023-04-10 22:58:03 +0300

    Решил задачу 2 (нахождение суммы цифр числа)

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+n = int(input("Введите трехзначное число: "))
+count = 0
+summ = 0
+len_n = len(str(n))
+while count < len_n:
+    summ += n%10
+    count += 1
+    n = int(n/10)
+print("Сумма цифр числа равна", summ)
